28d14c3b0752e9fd3e3ae3e563da4c4ec9ce0642
Syed1012 <syedmusaib66@gmail.com>
2024-02-26 21:16:33 +0530

    Login and signup changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,11 +5,68 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/assets/images/logo.svg" alt="LOGO" width={30} height={30} className="object-contain" />
+        <Image
+          src="/assets/images/logo.svg"
+          alt="LOGO"
+          width={100}
+          height={80}
+          className="object-contain"
+        />
       </Link>
+
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Log Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  {" "}
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
